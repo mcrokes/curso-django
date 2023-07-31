3f33a778e3ead3ac78282eddc73bab1ab720799c
Manuel <malfonso2108@gmail.com>
2023-07-30 23:28:24 -0400

    'DataTable' with ajax

--- core/erp/tests.py
+++ core/erp/tests.py
@@ -1,11 +1,17 @@
+import random
+
 from config.wsgi import *
 from core.erp.models import *
 
 data = ['Leche y derivados', 'Carnes, pescados y huevos', 'Patatas, legumbres, frutos secos',
         'Verduras y Hortalizas', 'Frutas', 'Cereales y derivados, azúcar y dulces',
         'Grasas, aceite y mantequilla']
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
+           'u', 'v', 'w', 'x', 'y', 'z']
 
-for i in data:
-    cat = Category(name=i)
-    cat.save()
-    print(f'Guardado reqistro Nro{cat.id}')
+for i in range(1,6000):
+    name= ''.join(random.choices(letters, k=5))
+    while Category.objects.filter(name=name).exists():
+        name = ''.join(random.choices(letters, k=5))
+    Category(name=name).save()
+    print(f'Guardado reqistro {i}')
