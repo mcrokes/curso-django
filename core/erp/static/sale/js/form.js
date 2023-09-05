2156f1bfa1a9759dfb6380d2dfbd0168f2bd3e1d
Manuel <malfonso2108@gmail.com>
2023-09-04 23:06:47 -0400

    "Sale Calculations"

--- core/erp/static/sale/js/form.js
+++ core/erp/static/sale/js/form.js
@@ -1,3 +1,82 @@
+var vents = {
+    items: {
+        cli: '',
+        date_joined: '',
+        subtotal: 0.00,
+        iva: 0.00,
+        total: 0.00,
+        products: []
+    },
+    calculate_invoice: function () {
+        var subtotal = 0.00
+        var iva = $('input[name="iva"]').val();
+        $.each(this.items.products, function (pos, dict) {
+            dict.subtotal = dict.cant * parseFloat(dict.pvp);
+            subtotal += dict.subtotal;
+        });
+        this.items.subtotal = subtotal;
+        this.items.iva = this.items.subtotal * iva;
+        this.items.total = this.items.subtotal + this.items.iva;
+
+        $('input[name="subtotal"]').val(this.items.subtotal.toFixed(2));
+        $('input[name="ivacalc"]').val(this.items.iva.toFixed(2));
+        $('input[name="total"]').val(this.items.total.toFixed(2));
+    },
+    add: function (item) {
+        this.items.products.push(item)
+        this.list()
+    },
+    list: function () {
+        this.calculate_invoice()
+        $('#tblProducts').DataTable({
+            responsive: true,
+            autoWidth: false,
+            destroy: true,
+            deferRender: true,
+            data: this.items.products,
+            columns: [
+                {"data": "id"},
+                {"data": "name"},
+                {"data": "cat.name"},
+                {"data": "pvp"},
+                {"data": "cant"},
+                {"data": "subtotal"},
+            ],
+            columnDefs: [
+                {
+                    targets: [0],
+                    class: 'text-center',
+                    orderable: false,
+                    render: function (data, type, row) {
+                        return buttons = '<a rel="remove" class="btn btn-danger btn-xs btn-flat"><i class="fas fa-trash-alt"></i></a>';
+                    },
+                },
+                {
+                    targets: [-3, -1],
+                    class: 'text-center',
+                    orderable: false,
+                    render: function (data, type, row) {
+                        return '$' + parseFloat(data).toFixed(2);
+                    }
+                },
+                {
+                    targets: [-2],
+                    class: 'text-center',
+                    orderable: false,
+                    render: function (data, type, row) {
+                        return '<input type="text" name="cant" class="form-control form-control-sm" autocomplete="off" value="' + row.cant + '">';
+                    }
+                }
+            ],
+
+            initComplete: function (settings, json) {
+
+            },
+
+        });
+    }
+}
+
 $(function () {
     $('.select2').select2({
         theme: "bootstrap4",
@@ -11,19 +90,55 @@ $(function () {
         //maxDate: moment().format("YYYY-DD-MM")
     });
 
-    $("input[name='iva']").TouchSpin({
-        min: 0,
-        max: 100,
-        step: 0.1,
-        decimals: 2,
-        boostat: 5,
-        maxboostedstep: 10,
-        postfix: '%',
-        buttondown_class: 'btn btn-secondary',
-        buttonup_class: 'btn btn-secondary',
-    })
+    $("input[name='iva']")
+        .TouchSpin({
+            min: 0,
+            max: 100,
+            step: 0.01,
+            decimals: 2,
+            boostat: 5,
+            maxboostedstep: 10,
+            postfix: '%',
+            buttondown_class: 'btn btn-secondary',
+            buttonup_class: 'btn btn-secondary',
+        })
+        .val(0.12)
+        .on('change', function () {
+            vents.calculate_invoice()
+        })
+
+    // search products
+
+    $('input[name="search"]').autocomplete({
+        source: function (request, response) {
+            $.ajax({
+                url: window.location.pathname,
+                type: 'POST',
+                data: {
+                    'action': 'search_products',
+                    'term': request.term
+                },
+                dataType: 'json',
+            }).done(function (data) {
+                response(data)
+            }).fail(function (jqXHR, textStatus, errorThrown) {
+                //alert(textStatus + ': ' + errorThrown);
+            }).always(function (data) {
+            });
+        },
+        delay: 500,
+        minLength: 1,
+        select: function (event, ui) {
+            event.preventDefault()
+            console.clear()
+            ui.item.cant = 1;
+            ui.item.subtotal = 0.00;
+            console.log(vents.items);
+
+            vents.add(ui.item);
+
+            $(this).val('')
+        }
+    });
 });
 
-$(function () {
-
-});
