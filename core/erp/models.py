fde84972a52eaee0434076fea2ac0b22d357190b
Manuel <malfonso2108@gmail.com>
2023-09-07 00:27:05 -0400

    "Saving Sale Details"

--- core/erp/models.py
+++ core/erp/models.py
@@ -101,6 +101,16 @@ class Sale(models.Model):
     def __str__(self):
         return self.cli.names
 
+    def toJSON(self):
+        item = model_to_dict(self)
+        item['cli'] = self.cli.toJSON()
+        item['data_joined'] = self.date_joined.strftime('%Y-%m-%d')
+        item['subtotal'] = format(self.subtotal, '.2f')
+        item['iva'] = format(self.iva, '.2f')
+        item['total'] = format(self.total, '.2f')
+        item['det'] = [i.toJSON() for i in self.detsale_set.all()]
+        return item
+
     class Meta:
         verbose_name = 'Venta'
         verbose_name_plural = 'Ventas'
@@ -116,6 +126,14 @@ class DetSale(models.Model):
 
     def __str__(self):
         return self.prod.name
+
+    def toJSON(self):
+        item = model_to_dict(self, exclude=['sale'])
+        # item['sale'] = self.sale.toJSON()
+        item['prod'] = self.prod.toJSON()
+        item['price'] = format(self.price, '.2f')
+        item['subtotal'] = format(self.subtotal, '.2f')
+        return item
 
     class Meta:
         verbose_name = 'Detalle de Venta'
--- core/erp/views/sale/views.py
+++ core/erp/views/sale/views.py
@@ -1,14 +1,52 @@
+import json
+
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.db import transaction
 from django.http import JsonResponse
 from django.urls import reverse_lazy
 from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_exempt
-from django.views.generic import CreateView
+from django.views.generic import CreateView, DeleteView, ListView
 
 from core.erp.forms import SaleForm
 from core.erp.mixins import ValidatePermissionRequiredMixin
-from core.erp.models import Sale, Product
+from core.erp.models import Sale, Product, DetSale
 
+
+class SaleListView(LoginRequiredMixin, ValidatePermissionRequiredMixin, ListView):
+    model = Sale
+    template_name = 'sale/list.html'
+    permission_required = 'erp.view_sale'
+
+    @method_decorator(csrf_exempt)
+    def dispatch(self, request, *args, **kwargs):
+        return super().dispatch(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        data = {}
+        try:
+            action = request.POST['action']
+            if action == 'searchdata':
+                data = []
+                for i in Sale.objects.all():
+                    data.append(i.toJSON())
+            elif action == 'search_details_prod':
+                data = []
+                for i in DetSale.objects.filter(sale_id=request.POST['id']):
+                    data.append(i.toJSON())
+            else:
+                data['error'] = 'Ha ocurrido un error'
+        except Exception as e:
+            data['error'] = str(e)
+        return JsonResponse(data, safe=False)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Listado de Ventas'
+        context['create_url'] = reverse_lazy('erp:sale_create')
+        context['list_url'] = reverse_lazy('erp:sale_list')
+        context['entity'] = 'Ventas'
+        return context
 
 class SaleCreateView(LoginRequiredMixin, ValidatePermissionRequiredMixin, CreateView):
     model = Sale
@@ -34,6 +72,27 @@ class SaleCreateView(LoginRequiredMixin, ValidatePermissionRequiredMixin, Create
                     item['value'] = i.name
                     # item['cant'] = 0
                     data.append(item)
+            elif action == 'add':
+                with transaction.atomic():
+                    vents = json.loads(request.POST['vents'])
+
+                    sale = Sale()
+                    sale.date_joined = vents['date_joined']
+                    sale.cli_id = (vents['cli'])
+                    sale.subtotal = float(vents['subtotal'])
+                    sale.iva = float(vents['iva'])
+                    sale.total = float(vents['total'])
+                    sale.save()
+
+                    for i in vents['products']:
+                        det = DetSale()
+                        det.sale_id = sale.id
+                        det.prod_id = i['id']
+                        det.cant = int(i['cant'])
+                        det.price = float(i['pvp'])
+                        det.subtotal = float(i['subtotal'])
+
+                        det.save()
             else:
                 data['error'] = 'No ha ingresado a ninguna opción'
         except Exception as e:
@@ -46,4 +105,31 @@ class SaleCreateView(LoginRequiredMixin, ValidatePermissionRequiredMixin, Create
         context['entity'] = 'Ventas'
         context['list_url'] = self.success_url
         context['action'] = 'add'
+        return context
+
+
+class SaleDeleteView(LoginRequiredMixin, ValidatePermissionRequiredMixin, DeleteView):
+    model = Sale
+    template_name = 'sale/delete.html'
+    success_url = reverse_lazy('erp:sale_delete')
+    permission_required = 'erp.delete_sale'
+    url_redirect = success_url
+
+    def dispatch(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        return super().dispatch(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        data = {}
+        try:
+            self.object.delete()
+        except Exception as e:
+            data['error'] = str(e)
+        return JsonResponse(data)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Eliminación de una Venta'
+        context['entity'] = 'Ventas'
+        context['list_url'] = self.success_url
         return context
